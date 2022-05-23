2b107392821c66aa3481721dddd46ea20cba4655
icarus64 <ellishogan95@gmail.com>
2022-05-22 22:56:20 -0400

    update

--- w3Schools/js-grabber.py
+++ w3Schools/js-grabber.py
@@ -16,21 +16,27 @@ for url in url_arr[0: 1]:
 			if len(tr.find_all("th")) > 0: 
 				bold = f'<b>{url.capitalize()} {tr.find("th").text}</b>'
 			else:		
+				ref = soup(requests.get(f'https://www.w3schools.com/jsref/{tr.find("a")["href"]}').content, 'html.parser')
+				b = [i.text for i in tr.find_all("td")]
 				try: 		
-					ref = soup(requests.get(f'https://www.w3schools.com/jsref/{tr.find("a")["href"]}').content, 'html.parser')
 					back_extended = ref.find("h2", text="Syntax").findNext().findChild("div").findChild("div")
 					front = f'{bold}: {back_extended}'
-					back_basic = f'{bold} that {[i.text for i in tr.find_all("td")][1]}'
-					additional_info = ""
-					example = ref.find("div", {"class": "w3-example"}).find("div", {"class": "w3-code"})
-					version = "es6"
-
-					cards.append("\n".join(f'{front} | {back_basic} | {back_extended} | {additional_info} | {example} | {version}'.splitlines()))
 				except Exception as e:
+					back_extended = ""
+					front = f'{bold}: {b[0]}'
 					print(e)
+
+				back_basic = f'{bold} that {b[1]}'
+				additional_info = ""
+				example = ref.find("div", {"class": "w3-example"}).find("div", {"class": "w3-code"})
+				version = "es6"
+
+				cards.append(" ".join(f'{front} | {back_basic} | {back_extended} | {additional_info} | {example} | {version}'.splitlines()))
 	try: 
 		mkdir(f'{getcwd()}/output/')
 	except FileExistsError:
 		pass	
+	
+	print(cards[0])
 	with open(f'{getcwd()}/output/{url}.txt', 'w') as file:
 		file.writelines(cards)
