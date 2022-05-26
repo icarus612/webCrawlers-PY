e1632f4b5559054fc8c278f36b7bf026f4ca70fb
icarus612 <ellishogan@gmail.com>
2022-05-25 20:49:08 -0400

    update

--- w3Schools/js-grabber.py
+++ w3Schools/js-grabber.py
@@ -3,39 +3,44 @@ from bs4 import BeautifulSoup as soup
 from os import mkdir, getcwd, mkdir
 import re
 
-# front, back, example, 
-url_arr = ['array', 'string', 'number', 'math', 'date', 'global', 'regexp', 'classes', 'error', 'boolean', 'operators', 'statements', 'json']
-try: 
-	mkdir(f'{getcwd()}/output/')
-except FileExistsError:
-	pass	
+home_page =  soup(requests.get('https://www.w3schools.com/jsref/default.asp').content, 'html.parser')
+url_arr = [[a['href'] for a in div.find_all('a')] for div in home_page.find_all("div", {'class': 'refcont'})][0:4]
+title_arr = [h2.text for h2 in home_page.find_all("h2")][0:4]
 
-for url in url_arr:
-	print(f'getting the content for {url}')
-	cards = []
-	res = soup(requests.get(f'https://www.w3schools.com/jsref/jsref_obj_{url}.asp').content, 'html.parser')
-	for table in res.find_all('table', {'class': 'ws-table-all'}):
-		bold = ''
-		for tr in table.find_all('tr'):
-			if len(tr.find_all('th')) > 0: 
-				bold = f'<b>{url.capitalize()} {tr.find("th").text.lower()}</b>'
-			else:		
-				ref = soup(requests.get(f'https://www.w3schools.com/jsref/{tr.find("a")["href"]}').content, 'html.parser')
-				b = [i.text for i in tr.find_all('td')]
-				try: 		
-					back_extended = ref.find('h2', text='Syntax').findNext().findChild('div').decode_contents().strip()
-					front = f'{bold}: {back_extended}'
-				except Exception as e:
-					back_extended = ''
-					front = f'{bold}: {b[0].lower()}'
-					print(e)
-				back_basic = f'{bold} that {b[1].lower()}.'
-				additional_info = ''
-				version = ''
-				try: 
-					example = ref.find('div', {'class': 'w3-example'}).find('div', {'class': 'w3-code'}).decode_contents().strip()
-				except AttributeError:
-					example = ''
-				cards.append(' '.join(f'{front} | {back_basic} | {back_extended} | {additional_info} | {example} | {version}'.splitlines()))
-	with open(f'{getcwd()}/output/{url}.txt', 'w') as file:
-		file.writelines([f'{i} \n' for i in cards])
+
+for idx, t in enumerate(title_arr):
+	title = "".join(t.title().split())
+	try: 
+		mkdir(f'{getcwd()}/output/{title}')
+	except FileExistsError:
+		pass	
+
+	for url in url_arr[idx]:
+		print(f'getting the content for {url}')
+		cards = []
+		res = soup(requests.get(f'https://www.w3schools.com/jsref/{url}.asp').content, 'html.parser')
+		for table in res.find_all('table', {'class': 'ws-table-all'}):
+			bold = ''
+			for tr in table.find_all('tr'):
+				if len(tr.find_all('th')) > 0: 
+					bold = f'<b>{url.capitalize()} {tr.find("th").text.lower()}</b>'
+				else:		
+					ref = soup(requests.get(f'https://www.w3schools.com/jsref/{tr.find("a")["href"]}').content, 'html.parser')
+					b = [i.text for i in tr.find_all('td')]
+					try: 		
+						back_extended = ref.find('h2', text='Syntax').findNext().findChild('div').decode_contents().strip()
+						front = f'{bold}: {back_extended}'
+					except Exception as e:
+						back_extended = ''
+						front = f'{bold}: {b[0].lower()}'
+						print(e)
+					back_basic = f'{bold} that {b[1].lower()}.'
+					additional_info = ''
+					version = ''
+					try: 
+						example = ref.find('div', {'class': 'w3-example'}).find('div', {'class': 'w3-code'}).decode_contents().strip()
+					except AttributeError:
+						example = ''
+					cards.append(' '.join(f'{front} | {back_basic} | {back_extended} | {additional_info} | {example} | {version}'.splitlines()))
+		with open(f'{getcwd()}/output/{title}/{url}.txt', 'w') as file:
+			file.writelines([f'{i} \n' for i in cards])
