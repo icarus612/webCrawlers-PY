26a3671af7472cc7c09848214e3c959c2d22178d
icarus612 <ellishogan@gmail.com>
2022-05-25 20:57:24 -0400

    update

--- w3Schools/js-grabber.py
+++ w3Schools/js-grabber.py
@@ -3,17 +3,20 @@ from bs4 import BeautifulSoup as soup
 from os import mkdir, getcwd, mkdir
 import re
 
+def make_folder(name): 
+	try: 
+		mkdir(f'{getcwd()}/{name}')
+	except FileExistsError:
+		pass	
+
 home_page =  soup(requests.get('https://www.w3schools.com/jsref/default.asp').content, 'html.parser')
 url_arr = [[a['href'] for a in div.find_all('a')] for div in home_page.find_all("div", {'class': 'refcont'})][0:4]
 title_arr = [h2.text for h2 in home_page.find_all("h2")][0:4]
-
+make_folder('output/')	
 
 for idx, t in enumerate(title_arr):
 	title = "".join(t.title().split())
-	try: 
-		mkdir(f'{getcwd()}/output/{title}')
-	except FileExistsError:
-		pass	
+	make_folder(f'output/{title}')	
 
 	for url in url_arr[idx]:
 		print(f'getting the content for {url}')
@@ -42,5 +45,5 @@ for idx, t in enumerate(title_arr):
 					except AttributeError:
 						example = ''
 					cards.append(' '.join(f'{front} | {back_basic} | {back_extended} | {additional_info} | {example} | {version}'.splitlines()))
-		with open(f'{getcwd()}/output/{title}/{url}.txt', 'w') as file:
+		with open(f'{getcwd()}/output/{title}/{url.split("_")[-1].split(".")[0]}.txt', 'w') as file:
 			file.writelines([f'{i} \n' for i in cards])
