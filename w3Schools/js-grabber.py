c518eb540c4ce2c3bc7848c5c5758f414374c637
icarus612 <ellishogan@gmail.com>
2022-05-27 21:35:22 -0400

    all

--- w3Schools/js-grabber.py
+++ w3Schools/js-grabber.py
@@ -10,26 +10,26 @@ def make_folder(name):
 		pass	
 
 home_page =  soup(requests.get('https://www.w3schools.com/jsref/default.asp').content, 'html.parser')
-url_arr = [[a['href'] for a in div.find_all('a')] for div in home_page.find_all("div", {'class': 'refcont'})]
-title_arr = ['Built-in Objects', 'Language Structure', 'DOM', 'Web Api', 'Window']
+url_arr = [[{"url": a['href'], "title": a.text}  for a in div.find_all('a')] for div in home_page.find_all("div", {'class': 'refcont'})]
+title_arr = ['Built-in Objects', 'Window', 'DOM', 'Web Api',]
 make_folder('output/')	
 
 for idx, t in enumerate(title_arr):
 	title = "".join(t.split())
 	make_folder(f'output/{title}')	
 	for url in url_arr[idx]:
-		print(f'getting the content for {url}')
+		print(f'getting the content for {url["title"]}')
 		cards = []
 		try:
-			res = soup(requests.get(f'https://www.w3schools.com/jsref/{url}').content, 'html.parser')
+			res = soup(requests.get(f'https://www.w3schools.com/jsref/{url["url"]}').content, 'html.parser')
 		except:
-			url_arr[idx].append(url)
+			url_arr[idx]["url"].append(url)
 			continue
 		for table in res.find_all('table', {'class': 'ws-table-all'}):
 			bold = ''
 			for tr in table.find_all('tr'):
 				if len(tr.find_all('th')) > 0: 
-					bold = f'<b>{url.capitalize()} {tr.find("th").text.lower()}</b>'
+					bold = f'<b>{url["title"].capitalize()} {tr.find("th").text.lower()}</b>'
 				elif tr.find('a'):		
 					ref = soup(requests.get(f'https://www.w3schools.com/jsref/{tr.find("a")["href"]}').content, 'html.parser')
 					b = [i.text for i in tr.find_all('td')]
@@ -48,5 +48,5 @@ for idx, t in enumerate(title_arr):
 					except AttributeError:
 						example = ''
 					cards.append(' '.join(f'{front} | {back_basic} | {back_extended} | {additional_info} | {example} | {version}'.splitlines()))
-		with open(f'{getcwd()}/output/{title}/{url.split("_")[-1].split(".")[0]}.txt', 'w') as file:
+		with open(f'{getcwd()}/output/{title}/{"".join(url["title"].split(" "))}.txt', 'w') as file:
 			file.writelines([f'{i} \n' for i in cards])
