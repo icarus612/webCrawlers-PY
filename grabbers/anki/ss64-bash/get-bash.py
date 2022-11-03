15ee85bea7107be208dd98cdb657ceb14eddc3f4
icarus64 <ellishogan95@gmail.com>
2022-11-02 21:53:23 -0400

    update

--- grabbers/anki/ss64-bash/get-bash.py
+++ grabbers/anki/ss64-bash/get-bash.py
@@ -3,14 +3,7 @@ from bs4 import BeautifulSoup as soup
 from os import mkdir, getcwd, mkdir
 import re
 
-def make_folder(name): 
-	try: 
-		mkdir(f'{getcwd()}/{name}')
-	except FileExistsError:
-		pass	
-
 page =  soup(requests.get('https://ss64.com/bash/').content, 'html.parser')
-make_folder('output/')	
 cards = []
 for el in page.find_all('tr'):
 	td = list(filter(lambda x: x != "\xa0", [e.text for e in el.find_all('td')]))
