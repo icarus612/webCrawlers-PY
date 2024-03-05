cdbc2097e4dc0e91c1e76b59824bbf2f83d6fac2
icarus64 <ellishogan95@gmail.com>
2024-03-04 23:59:04 -0500

    update

--- web_crawlers/anki_scrapers/golang/packages_downloader.py
+++ web_crawlers/anki_scrapers/golang/packages_downloader.py
@@ -3,35 +3,39 @@ from bs4 import BeautifulSoup as soup
 from sys import argv 
 from os import makedirs
 
-lib = argv[1] if len(argv) > 1 else 'builtin'
-#should_remove_start = len(argv) <= 2  or argv[2].lower() not in ['0', 'false', 'f', 'n', 'no']
-page =  soup(requests.get(f'https://pkg.go.dev/{lib}@go1.21.0').content, 'html.parser')
-decks= {}
 
-for el in [i.find('span') for i in page.find_all('h4') if 'func' in i.text]:
-  is_method = '(' in el.text 
-  deck_name = el.text.split('(')[1].split(')')[0].replace('*', '') if is_method else 'general'
-  with_options = el.find_next('div').find('pre').text
-  name = el.find("a").text.strip()
-  func_init = f'<i>{deck_name}</i>' if is_method else lib
-  params = ', '.join([f'<i>{x.split(" ")[0]}</i>' for x in with_options.partition(name + '(')[2].split(')')[0].split(', ')])
-  func = f'{func_init}.{name}({params})'
-  info_start = f'<b>{lib.capitalize()} function</b> that'
-  p_el = el.find_next('p')
-  info_end = p_el.text[len(name) + 1:].replace('\n', ' ').split(' ') if p_el else [' ']
-  info = info_start + ' ' + ' '.join(info_end)
-  card = [func, info, '', with_options]
-  
-  if deck_name in decks.keys():
-    decks[deck_name].append(card) 
-  else:
-    decks[deck_name] = [card]
+def download_lib(lib='builtin', version='1.21.0'):
+  page =  soup(requests.get(f'https://pkg.go.dev/{lib}@go{version}').content, 'html.parser')
+  decks= {}
 
-try:
-  makedirs(lib)
-except:
-  pass
+  for el in [i.find('span') for i in page.find_all('h4') if 'func' in i.text]:
+    is_method = '(' in el.text 
+    deck_name = el.text.split('(')[1].split(')')[0].replace('*', '') if is_method else 'general'
+    with_options = el.find_next('div').find('pre').text
+    name = el.find("a").text.strip()
+    func_init = f'<i>{deck_name}</i>' if is_method else lib
+    params = ', '.join([f'<i>{x.split(" ")[0]}</i>' for x in with_options.partition(name + '(')[2].split(')')[0].split(', ')])
+    func = f'{func_init}.{name}({params})'
+    info_start = f'<b>{lib.capitalize()} function</b> that'
+    p_el = el.find_next('p')
+    info_end = p_el.text[len(name) + 1:].replace('\n', ' ').split(' ') if p_el else [' ']
+    info = info_start + ' ' + ' '.join(info_end)
+    card = [func, info, '', with_options]
+    
+    if deck_name in decks.keys():
+      decks[deck_name].append(card) 
+    else:
+      decks[deck_name] = [card]
 
-for deck, cards in decks.items():
-  with open(f'{lib}/{deck}.txt', 'w') as file:
-    file.writelines([f'{" | ".join(i)} \n' for i in cards])	
+  try:
+    makedirs(lib)
+  except:
+    pass
+
+  for deck, cards in decks.items():
+    with open(f'{lib}/{deck}.txt', 'w') as file:
+      file.writelines([f'{" | ".join(i)} \n' for i in cards])	
+    
+if __name__ == '__main__':
+    lib, version = argv
+    download_lib(lib=lib, version=version)
---
+++ web_crawlers/anki_scrapers/vim/vim_rtorr.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup as soup
+from sys import argv 
+from os import makedirs
+
+
+def download_commands():
+	page =  soup(requests.get('https://vim.rtorr.com/').content, 'html.parser')
+	raw_decks = dict((i.text, i.find_next('ul').find_all('li')) for i in page.find_all('h2') if i.find_next().name == 'ul')
+	decks = dict()
+	try:
+		makedirs('workbench')
+	except:
+		pass
+
+	for deck, items in raw_decks.items():
+		cards = decks[deck] = []
+		for item in items:
+			txt = [i.strip() for i in item.text.split('-')]
+			kw = f'<b>Vim Command:</b> {txt[0]}'
+			info = f'<b>Vim command</b> used to {txt[1]}.'
+			cards.append(txt)
+   
+
+	for deck, cards in decks.items():
+		with open(f'workbench/{deck}.txt', 'w') as file:
+			file.writelines([f'{" | ".join(i)} \n' for i in cards])	
+		
+if __name__ == '__main__':
+	download_commands()
