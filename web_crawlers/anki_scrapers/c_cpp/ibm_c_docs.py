5fd6afde573e3cc4c9197330a3770c9072670d07
icarus64 <ellishogan95@gmail.com>
2024-03-05 22:31:15 -0500

    update

--- web_crawlers/anki_scrapers/c_cpp/ibm_c_docs.py
+++ web_crawlers/anki_scrapers/c_cpp/ibm_c_docs.py
@@ -7,17 +7,19 @@ from os import makedirs
 def download_commands():
 	page =  soup(requests.get('https://www.ibm.com/docs/en/i/7.3?topic=extensions-standard-c-library-functions-table-by-name').content, 'html.parser')
 	all_cards = [[x.text for x in i.find_all('td')] for i in page.find('tbody').find_all('tr') if len(i.find_all('td')) == 4]
-
+	print(len(all_cards))
 	deck_names = set()
 	for i in all_cards:
-		for d in i[1].split(' '):
-			deck_names.add(d)
+		for d in i[1].replace('\n', ' ').split(' '):
+			if len(d.strip()) > 0:
+				deck_names.add(d.strip())
 
 	decks = dict((i, []) for i in deck_names)
 	for card in all_cards:
-		for deck in card[1].split(' '):
-			decks[deck].append(card)
-			
+		for deck in card[1].replace('\n', ' ').split(' '):
+			if deck in decks:
+				decks[deck.strip()].append(card)
+
 	try:
 		makedirs('workbench')
 	except:
