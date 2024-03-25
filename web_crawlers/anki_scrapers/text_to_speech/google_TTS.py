385c38eaaeb5e89ca93fead57a6d3be5873546f6
icarus64 <ellishogan95@gmail.com>
2024-03-24 23:58:10 -0400

    update

--- web_crawlers/anki_scrapers/text_to_speech/google_TTS.py
+++ web_crawlers/anki_scrapers/text_to_speech/google_TTS.py
@@ -1,31 +1,58 @@
 import json
 import argparse
+from google_speech import Speech
+from os import path
+from retrying import retry
 
+@retry(wait_exponential_multiplier=1000, wait_exponential_max=40000)
 def TTS(word, language):
-	pass
+	return Speech(word, language)
 
 def load_file(file_name):
 	with open(file_name, 'r') as file:
-		text = file.read()
+		text = file.readlines()
 	return text
 
 def load_json_file(file_name):
-	text = load_text_file(file_name)
+	with open(file_name, 'r') as file:
+		text = file.read()	
 	return json.loads(text) 
+
+def save_audio(file_name, audio, replace=True):
+	if replace or not path.isfile(file_name):
+		with open(file_name, 'w') as file:
+			file.write(audio)
+
+def build_and_save(word, language, file_name=None, dir_name='audio_files', replace=True):
+	if not file_name:
+		file_name = path.join(dir_name, word.replace(' ', '_') + '.mp3')
+	audio = TTS(word, language)
+	save_audio(file_name, audio, replace=replace)
+
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('file_name', default='language_file.txt', type=str, help='The name of the file to load')
-	parser.add_argument('--language', '-l', default='russian', type=str, help='The language to use for the TTS')
-	parser.add_argument('--version', '-v', default='A', type=str, help='The speech version to use for the TTS')
-	parser.add_argument('--output', '-o', default='output', type=str, help='The name of the output file')
-
+	parser.add_argument('--word', '-w', type=str, help='The word to load')
+	parser.add_argument('--file', '-f', type=str, help='The name of the file to load')
+	parser.add_argument('--language', '-l', default='RU', type=str, help='The language to use for the TTS')
+	parser.add_argument('--output_dir', '-o', default='audio_files', type=str, help='The name of the output file')
+	parser.add_argument('--split_char', '-s', default='|', type=str, help='The name of the output file')
 	args = parser.parse_args()
 
-	if args.file_name.endswith('.json'):
-		text = load_json_file(args.file_name)
-	else:
-		text = load_text_file(args.file_name)
+	if args.word:
+		build_and_save(args.word, args.language, dir_name=args.output_dir)
 
-	for word in text:
-		TTS(word, args.language)
+	elif args.file:
+		if args.file.endswith('.json'):
+			cards = load_json_file(args.file_name).items()
+		else:
+			cards = load_file(args.file_name)
+			if args.split_char:
+				cards = [i.split(args.split_char) for i in cards]
+	
+		for card in cards:
+			if len(card) == 1:
+				build_and_save(word, args.language, dir_name=args.output_dir) 
+			else:
+				build_and_save(word, args.language, file_name=file, dir_name=args.output_dir) 
+		
